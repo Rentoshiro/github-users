7993cedf0c8a4d67a285cace9792324caac47af7
Rentoshiro <149387302+Rentoshiro@users.noreply.github.com>
2024-03-10 20:39:49 +0100

    added user

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
 
-const Pie3D = () => {
-  return <div>chart</div>;
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "pie2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      chart: {
+        caption: "Languages",
+        theme: "fusion",
+        decimals: 1,
+        pieRadius: "35%",
+      },
+      data,
+    },
+  };
+  return (
+    <>
+      <ReactFC {...chartConfigs} />
+    </>
+  );
 };
 
-export default Pie3D;
+export default ChartComponent;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,6 +14,7 @@ const Login = () => {
     </Wrapper>
   );
 };
+
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
